86417adc105fd01d584426d73568f6d3363cc23a
Jon Urry <github@agileweb.co.uk>
2018-02-16 13:48:21 +0000

    Loading from github gists. Syntax highlighting.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,62 @@
-var myCodeMirror = CodeMirror(document.body, {
-  value: "function myScript(){return 100;}\n",
-  mode: "javascript"
-});
+const gistsURL = 'https://api.github.com/users/jonurry/gists';
 
-function renderGists(gists) {
-  for (let gist of gists) {
-    document.write("<p>" + gist.id + "</p>");
-    console.log(gist.comments_url);
+function createCodeElement(gist) {
+  let solutionsElement = document.getElementById('solutions');
+  let newElement = solutionsElement.appendChild(document.createElement('div'));
+  newElement.setAttribute('id', gist.id);
+  let h1 = newElement.appendChild(document.createElement('h1'));
+  h1.innerHTML = gist.description;
+  return newElement;
+}
+
+async function apiGetJson(url) {
+  return await (await fetch(url)).json();
+}
+
+async function apiGetText(url) {
+  return await (await fetch(url)).text();
+}
+
+async function renderCode(code, element) {
+  element.innerHTML = marked('```js\n' + code + '\n```');
+  hljs.highlightBlock(element.firstChild.firstChild);
+}
+
+async function renderComments(comments, id) {
+  let first = true;
+  for (let comment of comments) {
+    let commentElement;
+    let gistElement = document.getElementById(id);
+    if (first) {
+      gistElement.prepend(document.createElement('div'));
+      commentElement = gistElement.firstChild;
+      first = false;
+    } else {
+      commentElement = gistElement.appendChild(document.createElement('div'));
+    }
+    commentElement.setAttribute('id', comment.id);
+    commentElement.innerHTML = marked(comment.body);
   }
 }
 
-fetch("https://api.github.com/users/jonurry/gists")
-  .then(resp => resp.json()) // Transform response data to json
-  .then(function(data) {
-    // Here you get the data to modify as you please
-    renderGists(data);
-  })
-  .catch(function(error) {
-    // If there is any error you will catch them here
-    console.log(error);
-  });
+async function render() {
+  try {
+    let gists = await apiGetJson(gistsURL);
+    for (let gist of gists) {
+      let e = createCodeElement(gist);
+      let codeURL = gist.files[Object.keys(gist.files)[0]].raw_url;
+      let code = await apiGetText(codeURL);
+      await renderCode(code, e);
+      if (gist.comments > 0) {
+        let comments = await apiGetJson(gist.comments_url);
+        await renderComments(comments, gist.id);
+      }
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+(async () => {
+  await render();
+})();
